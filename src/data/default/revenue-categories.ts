59bae06e6e9ec2e04c1351fb84a2924e562a547f
mendoza000 <omarmendoza.alv25@gmail.com>
2024-02-15 15:56:30 -0400

    add default revenue categories

---
+++ src/data/default/revenue-categories.ts
@@ -0,0 +1,22 @@
+export const defaultRevenueCategories = [
+  {
+    id: "1",
+    name: "Salary",
+    icon: "💵",
+  },
+  {
+    id: "2",
+    name: "Freelance",
+    icon: "📝",
+  },
+  {
+    id: "3",
+    name: "Investment",
+    icon: "📈",
+  },
+  {
+    id: "4",
+    name: "Others",
+    icon: "🛠️",
+  },
+]
