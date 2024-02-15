4357d5a1720927504db6931dce3fdac43374edaa
mendoza000 <omarmendoza.alv25@gmail.com>
2024-02-15 15:44:20 -0400

    add default categories list

---
+++ src/data/default/categories.ts
@@ -0,0 +1,37 @@
+export const defaultCategories = [
+  {
+    id: "1",
+    name: "Food",
+    icon: "🍔",
+  },
+  {
+    id: "2",
+    name: "Transport",
+    icon: "🚗",
+  },
+  {
+    id: "3",
+    name: "Shopping",
+    icon: "🛒",
+  },
+  {
+    id: "4",
+    name: "Entertainment",
+    icon: "🎮",
+  },
+  {
+    id: "5",
+    name: "Health",
+    icon: "🏥",
+  },
+  {
+    id: "6",
+    name: "Education",
+    icon: "📚",
+  },
+  {
+    id: "7",
+    name: "Others",
+    icon: "🛠️",
+  },
+];
