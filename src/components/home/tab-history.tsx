ff8526ce0e7a4d4d3c88e4e110a9474e20ae555e
mendoza000 <omarmendoza.alv25@gmail.com>
2024-02-16 16:46:42 -0400

    add dynamic history

--- src/components/home/tab-history.tsx
+++ src/components/home/tab-history.tsx
@@ -1,23 +1,27 @@
 import { CardBody } from "@nextui-org/react";
 import HistoryItem from "./history-item";
+import useGetHistory from "../../hooks/useGetHistory";
 
 export default function TabHistory() {
+	const history = useGetHistory();
+
 	return (
-		<CardBody className="gap-3">
-			<HistoryItem
-				type="expense"
-				amount={10}
-				category="Food"
-				title="Buy coffee"
-			/>
-			<HistoryItem
-				type="expense"
-				amount={15}
-				category="Food"
-				title="Buy burger"
-			/>
-			<HistoryItem type="revenue" amount={3} category="Job" title="Sell code" />
-			<HistoryItem type="revenue" amount={3} category="Job" title="Sell code" />
+		<CardBody className="gap-3 min-h-full">
+			{history.length < 1 && (
+				<p className="text-center my-auto flex mx-auto text-default-500">
+					No hay movimientos aun :/
+				</p>
+			)}
+			{history.map((e) => {
+				return (
+					<HistoryItem
+						type={e.type}
+						amount={e.amount}
+						category={e.category.id}
+						title={e.title}
+					/>
+				);
+			})}
 		</CardBody>
 	);
 }
