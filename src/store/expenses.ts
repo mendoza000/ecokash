6d25db138d7544f02b0ee44c4e00b3dfd4b4d7e4
mendoza000 <omarmendoza.alv25@gmail.com>
2024-02-16 15:40:24 -0400

    add store

---
+++ src/store/expenses.ts
@@ -0,0 +1,39 @@
+import {create} from "zustand"
+import type { Expense } from "../types";
+import { devtools, persist } from "zustand/middleware";
+
+interface ExpensesState {
+  expenses: Expense[];
+  setExpenses: (expenses: Expense[]) => void;
+  addExpense: (Expense: Expense) => void;
+  removeExpense: (id: string) => void;
+}
+
+export const useExpensesStore = create<ExpensesState>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        expenses: [],
+        setExpenses: (expenses) => set((state) => {
+          return {
+            ...state,
+            expenses,
+          }
+        }),
+        addExpense: (expense) => set((state) => {
+          return {
+            ...state,
+            expenses: [...state.expenses, expense],
+          }
+        }),
+        removeExpense: (id) => set((state) => {
+          return {
+            ...state,
+            revenues: state.expenses.filter((expense) => expense.id !== id),
+          }
+        }),
+      }),
+      {name: "ek-e"}
+    )
+  )
+)
---
+++ src/store/revenues.ts
@@ -0,0 +1,39 @@
+import {create} from "zustand"
+import type { Revenue } from "../types";
+import { devtools, persist } from "zustand/middleware";
+
+interface RevenuesState {
+  revenues: Revenue[];
+  setRevenues: (revenues: Revenue[]) => void;
+  addRevenue: (revenue: Revenue) => void;
+  removeRevenue: (id: string) => void;
+}
+
+export const useRevenuesStore = create<RevenuesState>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        revenues: [],
+        setRevenues: (revenues) => set((state) => {
+          return {
+            ...state,
+            revenues,
+          }
+        }),
+        addRevenue: (revenue) => set((state) => {
+          return {
+            ...state,
+            revenues: [...state.revenues, revenue],
+          }
+        }),
+        removeRevenue: (id) => set((state) => {
+          return {
+            ...state,
+            revenues: state.revenues.filter((revenue) => revenue.id !== id),
+          }
+        }),
+      }),
+      {name: "ek-r"}
+    )
+  )
+)
