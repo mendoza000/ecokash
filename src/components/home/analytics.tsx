86b0ed45dbe1d7fa85867c4c3415a0df9d7c3bfa
mendoza000 <omarmendoza.alv25@gmail.com>
2024-02-21 21:18:06 -0400

    show total balance

--- src/components/home/analytics.tsx
+++ src/components/home/analytics.tsx
@@ -1,7 +1,11 @@
-import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
-import { IconPigMoney, IconCashOff } from "@tabler/icons-react";
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import useGetHistory from "../../hooks/useGetHistory";
+import { formatToUs } from "../../lib/format-to-us";
+import useGetTotalAmount from "../../hooks/useGetTotalAmount";
 
 export default function Analytics() {
+	const total = useGetTotalAmount();
+
 	return (
 		<div className="grid grid-cols-1 gap-2 md:grid-cols-2">
 			<Card className="bg-gradient-to-tr from-blue-500 via-blue-500 to-blue-300">
@@ -10,7 +14,7 @@ export default function Analytics() {
 				</CardHeader>
 
 				<CardBody>
-					<span className="text-4xl font-bold">$ 300.00</span>
+					<span className="text-4xl font-bold">{formatToUs(total)}</span>
 				</CardBody>
 			</Card>
 
